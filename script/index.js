02c3cb3764dc077e2b832d00298d68b8477e8fe4
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-01-10 13:48:05 -0800

    Added dialog.

--- script/index.js
+++ script/index.js
@@ -1,7 +1,19 @@
-﻿require(["esri/map", "esri/layers/osm", "dojo/domReady!"], function () {
-	var map;
+﻿require(["dojo/on", "dijit/Dialog", "esri/map", "esri/layers/osm", "dojo/domReady!"], function (on, Dialog) {
+	var map, agsServiceDialog;
+
 
 	map = new esri.Map("map", {
 		basemap: "osm"
+	});
+
+	
+	agsServiceDialog = new Dialog({
+		title: "Title Goes Here",
+		content: "Here's the content",
+		style: "width: 300px"
+	});
+
+	on(dojo.byId("addDataButton"), "click", function () {
+		agsServiceDialog.show();
 	});
 });
