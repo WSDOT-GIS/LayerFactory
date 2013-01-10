e6fb113e1151b4c41323ea53ad78904f9ad7492a
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-01-10 14:38:16 -0800

    Added support for adding ArcGISDynamicMapServiceLayer and FeatureLayer services.

--- script/index.js
+++ script/index.js
@@ -1,5 +1,26 @@
-﻿require(["dojo/on", "dijit/Dialog", "esri/map", "esri/layers/osm", "dojo/domReady!"], function (on, Dialog) {
-	var map, agsServiceDialog;
+﻿require(["dojo/on", "dojo/dom", "dijit/Dialog", "esri/map", "esri/layers/agsdynamic", "esri/layers/featureLayer", "esri/layers/osm", "dojo/domReady!"], function (on, dom, Dialog) {
+	var map, agsServiceDialog, agsUrlInput;
+
+	function addAgsLayer(url) {
+		/// <summary>Adds an ArcGIS Service layer to a map.</summary>
+		/// <param name="url" type="String">URL to a map service.</param>
+
+		var mapServerRe, featureLayerRe, layer;
+		mapServerRe = /MapServer\/?$/i;
+		featureLayerRe = /MapServer\/\d+\/?$/i;
+
+		if (mapServerRe.test(url)) {
+			layer = new esri.layers.ArcGISDynamicMapServiceLayer(url);
+		} else if (featureLayerRe.test(url)) {
+			layer = new esri.layers.FeatureLayer(url);
+		}
+		if (layer) {
+			map.addLayer(layer);
+		}
+
+		return layer;
+
+	}
 
 
 	map = new esri.Map("map", {
@@ -8,12 +29,20 @@
 
 	
 	agsServiceDialog = new Dialog({
-		title: "Title Goes Here",
-		content: "Here's the content",
-		style: "width: 300px"
+		title: "Add a map service",
+		content: "<label for='agsUrlInput'>ArcGIS Map Service URL</label><input id='agsUrlInput' type='url' /><button id='addAgsLayerButton'>Add</button>",
+		resizable: true
 	});
 
-	on(dojo.byId("addDataButton"), "click", function () {
+	agsUrlInput = dom.byId("agsUrlInput");
+	agsUrlInput.value = "http://faaservices-1551414968.us-east-1.elb.amazonaws.com/ArcGIS/rest/services/201101_AirportsGIS_BH/Obstacles/MapServer";
+
+	on(dom.byId("addDataButton"), "click", function () {
 		agsServiceDialog.show();
+	});
+
+	on(dom.byId("addAgsLayerButton"), "click", function () {
+		addAgsLayer(agsUrlInput.value);
+		agsServiceDialog.hide();
 	});
 });
