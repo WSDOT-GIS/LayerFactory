6555af4fd17f5fc8f9507c64b08fb09a87ac1788
JeffJacobson <JeffJacobson@users.noreply.github.com>
2014-06-24 10:54:38 -0700

    Made LayerFactory.js AMD compliant. Upgraded ArcGIS API from 3.3 to 3.9.

--- script/wsdot/LayerFactory.js
+++ script/wsdot/LayerFactory.js
@@ -1,9 +1,73 @@
-﻿/*global define, esri*/
+﻿/*global define*/
 /*jslint nomen:true*/
 /// <reference path="../jsapi_vsdoc12_v33.js" />
-define(["require", "dojo/_base/declare", "dojo/Evented", "dojo/io/script"], function (require, declare, Evented) {
+define([
+	"require",
+	"dojo/_base/declare",
+	"dojo/Evented",
+	"esri/request",
+	"esri/layers/WebTiledLayer",
+	"esri/layers/OpenStreetMapLayer",
+	"esri/layers/FeatureLayer",
+	"esri/layers/ArcGISTiledMapServiceLayer",
+	"esri/layers/ArcGISDynamicMapServiceLayer",
+	"esri/layers/ArcGISImageServiceLayer",
+	"dojo/io/script"
+], function (require, declare, Evented, esriRequest, WebTiledLayer, OpenStreetMapLayer, FeatureLayer, ArcGISTiledMapServiceLayer, ArcGISDynamicMapServiceLayer, ArcGISImageServiceLayer) {
 	"use strict";
 	var layerFactory;
+
+	/**
+	 * Creates an OpenCycleMap WebTiledLayer
+	 * @param {string} type - Valid values: "Transport", "Landscape", "default"
+	 * @returns {esri/layers/WebTiledLayer}
+	 */
+	function CreateOpenCycleMapLayer(type) {
+		var subdomains = ["a", "b", "c"], urlTemplate, id, layer;
+
+		// Set the URL template.
+		if (/Transport/i.test(type)) {
+			urlTemplate = "http://${subDomain}.tile2.opencyclemap.org/transport/${level}/${col}/${row}.png";
+			id = "openCycleMapTransport";
+		} else if (/Landscape/i.test(type)) {
+			urlTemplate = "http://${subDomain}.tile3.opencyclemap.org/landscape/${level}/${col}/${row}.png";
+			id = "openCycleMapLandscape";
+		} else {
+			urlTemplate = "http://${subDomain}.tile.opencyclemap.org/cycle/${level}/${col}/${row}.png";
+			id = "openCycleMap";
+		}
+
+		layer = new WebTiledLayer(urlTemplate, {
+			id: id,
+			subDomains: subdomains,
+			copyright: '© OpenStreetMap contributors'
+		});
+
+		return layer;
+	}
+
+	/**
+	 * Creates a MapQuest layer.
+	 * @param {string} [type] - Set this to "Aerial" for MapQuest OpenAerial tiles, any other value (incl. undefined) for MapQuestOSM.
+	 * @returns {esri/layers/WebTiledLayer}
+	 */
+	function createMapQuestLayer(type) {
+		var layer, subdomains = ["otile1", "otile2", "otile3", "otile4"], ext = "png";
+		if (!!type && /Aerial/i.test(type)) {
+			layer = new WebTiledLayer("http://${subDomain}.mqcdn.com/tiles/1.0.0/sat/${level}/${col}/${row}." + ext, {
+				"id": "mapQuestOpenAerial",
+				"subDomains": subdomains,
+				"copyright": 'Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
+			});
+		} else {
+			layer = new WebTiledLayer("http://${subDomain}.mqcdn.com/tiles/1.0.0/map/${level}/${col}/${row}." + ext, {
+				"id": "mapQuestOSM",
+				"subDomains": subdomains,
+				"copyright": '© OpenStreetMap contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
+			});
+		}
+		return layer;
+	}
 
 	layerFactory = declare([Evented], {
 		constructor: function (/*options*/) {
@@ -20,7 +84,7 @@ define(["require", "dojo/_base/declare", "dojo/Evented", "dojo/io/script"], func
 			});
 		},
 		
-		/** 
+		/** Creates a layer. Once the layer is created an event will be triggered.
 		* @param {Object} options The options provided will vary depending on the type of layer to be created.
 		* @param {String} [options.url] The URL used to define service types such as ArcGISTiledMapServiceLayer, ArcGISDynamicServiceLayer, and ArcGISImageServiceLayer.
 		* @param {Object} [options.options] Layer constructors that take an options parameter can have those options provided via this property.
@@ -56,7 +120,7 @@ define(["require", "dojo/_base/declare", "dojo/Evented", "dojo/io/script"], func
 					if (!layerOptions.id) {
 						layerOptions.id = getMapNameFromUrl(url);
 					}
-					esri.request({
+					esriRequest({
 						url: url,
 						content: {
 							f: "json"
@@ -66,87 +130,26 @@ define(["require", "dojo/_base/declare", "dojo/Evented", "dojo/io/script"], func
 					}).then(function (response) {
 						if (response.singleFusedMapCache) {
 							// Create a tile map service layer.
-							require(["esri/layers/agstiled"], function () {
-								var layer;
-								layer = new esri.layers.ArcGISTiledMapServiceLayer(url, layerOptions);
-								self._triggerLayerCreate(layer);
-							});
+							self._triggerLayerCreate(new ArcGISTiledMapServiceLayer(url, layerOptions));
 						} else {
 							// Create a dynamic map service layer.
-							require(["esri/layers/agsdynamic"], function () {
-								var layer;
-								layer = new esri.layers.ArcGISDynamicMapServiceLayer(url, layerOptions);
-								self._triggerLayerCreate(layer);
-							});
+							self._triggerLayerCreate(new ArcGISDynamicMapServiceLayer(url, layerOptions));
 						}
 					}, function (error) {
 						self._triggerError(error);
 					});
 				} else if (featureLayerRe.test(url)) {
-					require(["esri/layers/FeatureLayer"], function () {
-						var layer;
-						layer = new esri.layers.FeatureLayer(url, layerOptions);
-						self._triggerLayerCreate(layer);
-					});
+					self._triggerLayerCreate(new FeatureLayer(url, layerOptions));
 				} else if (imageServerRe.test(url)) {
-					require(["esri/layers/agsimageservice"], function () {
-						var layer;
-						layer = new esri.layers.ArcGISImageServiceLayer(url, layerOptions);
-						self._triggerLayerCreate(layer);
-					});
+					self._triggerLayerCreate(new ArcGISImageServiceLayer(url, layerOptions));
 				}
 			} else if (options.type) {
 				if (/^OpenStreetMap$/i.test(options.type)) {
-					require(["esri/layers/osm"], function () {
-						var layer;
-						layer = new esri.layers.OpenStreetMapLayer(layerOptions);
-						self._triggerLayerCreate(layer);
-					});
+					self._triggerLayerCreate(new OpenStreetMapLayer(layerOptions));
 				} else if (/^MapQuest/i.test(options.type)) { // /^MapQuest\s?((O(?:pen)?S(?:treet)?M(?:ap)?)|(Open\s?Aerial))$/i.test(options.type)) {
-					require(["esri/layers/WebTiledLayer"], function () {
-						var layer, subdomains = ["otile1", "otile2", "otile3", "otile4"], ext = "png";
-						if (/Aerial/i.test(options.type)) {
-							layer = new esri.layers.WebTiledLayer("http://${subDomain}.mqcdn.com/tiles/1.0.0/sat/${level}/${col}/${row}." + ext, {
-								"id": "mapQuestOpenAerial",
-								"subDomains": subdomains,
-								"copyright": 'Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
-							});
-							self._triggerLayerCreate(layer);
-						} else {
-							layer = new esri.layers.WebTiledLayer("http://${subDomain}.mqcdn.com/tiles/1.0.0/map/${level}/${col}/${row}." + ext, {
-								"id": "mapQuestOSM",
-								"subDomains": subdomains,
-								"copyright": '© OpenStreetMap contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
-							});
-							self._triggerLayerCreate(layer);
-						}
-					});
+					self._triggerLayerCreate(createMapQuestLayer(options.type));
 				} else if (/^OpenCycleMap/i.test(options.type)) {
-					require(["esri/layers/WebTiledLayer"], function () {
-						var subdomains = ["a", "b", "c"], urlTemplate, id, layer;
-
-						// Set the URL template.
-						if (/Transport/i.test(options.type)) {
-							urlTemplate = "http://${subDomain}.tile2.opencyclemap.org/transport/${level}/${col}/${row}.png";
-							id = "openCycleMapTransport";
-						} else if (/Landscape/i.test(options.type)) {
-							urlTemplate = "http://${subDomain}.tile3.opencyclemap.org/landscape/${level}/${col}/${row}.png";
-							id = "openCycleMapLandscape";
-						} else {
-							urlTemplate = "http://${subDomain}.tile.opencyclemap.org/cycle/${level}/${col}/${row}.png";
-							id = "openCycleMap";
-						}
-
-						layer = new esri.layers.WebTiledLayer(urlTemplate, {
-							id: id,
-							subDomains: subdomains,
-							copyright: '© OpenStreetMap contributors'
-						});
-
-						self._triggerLayerCreate(layer);
-
-					});
-
+					self._triggerLayerCreate(CreateOpenCycleMapLayer(options.type));
 				}
 				
 			}
